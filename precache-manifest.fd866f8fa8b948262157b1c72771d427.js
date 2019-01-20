59df9d957d6fe09a57a8b6e5f2ca29e68e194897
romannep <neproman@gmail.com>
2019-01-20 17:36:19 +0400

    1

---
+++ precache-manifest.fd866f8fa8b948262157b1c72771d427.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfb749fb41edc5c53626b50ec9d48776",
+    "url": "./assets/images/loader-bg.png"
+  },
+  {
+    "revision": "66e209e9107b78c2739d",
+    "url": "./js/bundle.js"
+  },
+  {
+    "revision": "a065f116c71ddd04a413",
+    "url": "./js/vendor.bundle.js"
+  },
+  {
+    "revision": "af4a2d204ad788525c1673856c981f9e",
+    "url": "./assets/images/icon_run.png"
+  },
+  {
+    "revision": "8653f7d5ffe65a274e3a046e52aa0805",
+    "url": "./assets/images/tile-ground.jpg"
+  },
+  {
+    "revision": "f92c8ac1b4e6341908baa1d77f0dbebd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c97d238339edea4c33afd5a7f6a0d8ce",
+    "url": "./assets/images/icon-512px.png"
+  },
+  {
+    "revision": "8c484767e0591ca547fc8ddc12a9398d",
+    "url": "./assets/images/icon_delete.png"
+  },
+  {
+    "revision": "04abbef870f01da81cf8299975881a20",
+    "url": "./assets/images/tile-bg.jpg"
+  },
+  {
+    "revision": "6ec22d288ce9520f0c8dc36b6a259e23",
+    "url": "./assets/images/icon_inc.png"
+  },
+  {
+    "revision": "b58461f6e45b3d34dd7ec0c624b2712b",
+    "url": "./assets/images/icon_tube.png"
+  },
+  {
+    "revision": "8a8b0f0e976aaf5455c75c684ecc98bd",
+    "url": "./assets/images/steeltube24.png"
+  },
+  {
+    "revision": "78ea0555ef7648bf378481a5d778d344",
+    "url": "./assets/images/loader-bar.png"
+  },
+  {
+    "revision": "756472429bf526fcbbac2540d397807a",
+    "url": "./assets/images/steeltube32.png"
+  },
+  {
+    "revision": "720af0893cf8b64f6a7e04d45ff872d3",
+    "url": "./assets/images/icon_edit.png"
+  },
+  {
+    "revision": "c53758a5d58ed177f8e9d18ab80ed0fc",
+    "url": "./assets/images/steeltube16.png"
+  },
+  {
+    "revision": "bfd7f2fb9488329e9f3e8ca1993118ca",
+    "url": "./assets/images/mushroom2.png"
+  },
+  {
+    "revision": "09219c5b7d390e570d32b34aea5386a6",
+    "url": "./assets/images/steelball32.png"
+  },
+  {
+    "revision": "f8ba06c323a8fc17c0044df6df948a1f",
+    "url": "./assets/images/phaser-es6-webpack.jpg"
+  },
+  {
+    "revision": "7181949b2e8bd3553460b50ff9ad4078",
+    "url": "./assets/images/icon_dec.png"
+  },
+  {
+    "revision": "37b814ddea3f0cc8eed5c3985fc26e8f",
+    "url": "./assets/images/icon_ball.png"
+  },
+  {
+    "revision": "6eb0c377e0ea2aac41ac8d8b229a8020",
+    "url": "./assets/images/icon-192px.png"
+  },
+  {
+    "revision": "bb57ae8a7f547311f84722f8106f552d",
+    "url": "././manifest.json"
+  }
+];
